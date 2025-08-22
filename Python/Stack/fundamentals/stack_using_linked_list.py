ebbc2b23c2c6a9d3af686a118b93bd11c8cc64a4
Akash-ninja <akash.gitcode@gmail.com>
2025-08-22 16:22:51 +0530

    Stack using linked list in python

--- Python/Stack/fundamentals/stack_using_linked_list.py
+++ Python/Stack/fundamentals/stack_using_linked_list.py
@@ -1,1 +1,37 @@
 from Linked_List.fundamentals.linked_list_program import LinkedList
+
+
+class Stack:
+    def __init__(self):
+        self.size = 0
+        self.ll = LinkedList()
+
+    def top(self):
+        if self.size > 0:
+            return self.ll.head.data
+        return None
+
+    def push(self, value):
+        self.ll.insert_at_front(value)
+        self.size += 1
+
+    def pop(self):
+        self.ll.delete_at_front()
+        self.size -= 1
+
+
+st = Stack()
+
+st.push(100)
+st.push(300)
+st.push(50)
+
+print(f"Top element is = {st.top()}")
+
+st.pop()
+
+print(f"Top element is = {st.top()}")
+
+st.pop()
+
+print(f"Top element is = {st.top()}")
